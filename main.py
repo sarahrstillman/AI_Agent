0ed1f649cbab3f3b5cad9d9b26b0632af02ced44
Sarah Stillman <sarahr.stillman@gmail.com>
2025-11-14 13:09:40 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+if len(sys.argv) < 2:
+    print("Error: You must provide a prompt as a command line argument.")
+    print('Example: uv run main.py "Why are episodes 7-9 so much worse than 1-6?"')
+    sys.exit(1)
+
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages)
+
+print(response.text)
+
+usage = response.usage_metadata
+print(f"Prompt tokens: {usage.prompt_token_count}")
+print(f"Response tokens: {usage.candidates_token_count}")
+
+def main():
+    print("Hello from ai-agent!")
+
+
+if __name__ == "__main__":
+    main()
